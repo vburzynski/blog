26923a5dd216e77c5158d12d86029b38c0b656bf
Valerie Burzynski <valerie@bendyworks.com>
2023-03-08 19:21:25 -0600

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -166,5 +166,38 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-4GJFNXPGBK',
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // This is for Google Optimize for A/B Testing
+        // gtagConfig: {
+        //   optimize_id: 'OPT_CONTAINER_ID',
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: [
+            '/preview/**',
+            '/do-not-track/me/too/',
+          ],
+          // Defaults to https://www.googletagmanager.com
+          origin: 'YOUR_SELF_HOSTED_ORIGIN',
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
   ],
 };
