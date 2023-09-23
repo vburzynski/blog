88ec3547b0d2b48a87c9cc0445e085e7287ba0d7
Valerie Burzynski <valerie@bendyworks.com>
2023-09-23 10:11:16 -0500

    fix: insert/removed diff styling

---
+++ src/components/head.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+export default function Head({ title, description, image }) {
+  return (
+    <>
+      <meta charSet="utf-8" />
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
+      {description && <meta name="description" property="og:description" content={description} />}
+      {image && <meta property="og:image" content={image.url} />}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={title} />
+      {description && <meta name="twitter:description" content={description} />}
+      {image && <meta name="twitter:image" content={image.url} />}
+    </>
+  );
+}
